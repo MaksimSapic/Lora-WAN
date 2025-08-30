bbf6e87e64f390f5bd745359a548b18b766d2099
Lalicc <82774913+Lalicc@users.noreply.github.com>
2025-08-30 12:52:49 +0200

    Reciever for local network
    working reciever for local network

---
+++ src/reciever/reciever.ino
@@ -0,0 +1,30 @@
+#include <SPI.h>
+#include <LoRa.h>
+#define LORA_SCK 5
+#define LORA_MISO 18
+#define LORA_MOSI 23
+#define LORA_SS 17
+#define LORA_RST 14
+#define LORA_DIO0 26
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);  
+  SPI.begin(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS); 
+  LoRa.setPins(LORA_SS, LORA_RST, LORA_DIO0);  
+  if (!LoRa.begin(868E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }  
+  Serial.println("LoRa Receiver Initialized");
+}
+void loop() {
+  int packetSize = LoRa.parsePacket();
+  if (packetSize){    
+    Serial.print("Received packet: ");
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+    } 
+    Serial.print(" with RSSI ");
+    Serial.println(LoRa.packetRssi());  
+  }
+} 
